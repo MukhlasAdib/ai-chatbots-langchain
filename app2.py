8da0d1f908376aee88662372dc2bcc885cac1455
Mukhlas Adib <adib.rasyidy@gmail.com>
2026-07-17 20:50:20 +0700

    next

--- app2.py
+++ app2.py
@@ -1,4 +1,18 @@
+"""
+Applikasi streamlit chatbot
+
+Cara jalankan:
+>>> streamlit run app2.py
+"""
+
 import streamlit as st
 
 st.title("Adib Chatbot")
 st.markdown("Hi! Saya adib. Silahkan chat dengan AI assistant saya ya...")
+
+nama = st.text_input("Nama: ")
+st.markdown(f"Halo {nama}!")
+
+is_pressed = st.button("Tekan Untuk Keluarin Balon")
+if is_pressed:
+    st.balloons()
