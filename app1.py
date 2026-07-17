0d10d144a9ea9ae9baf94ab9cbab4bc0017cccb5
Mukhlas Adib <adib.rasyidy@gmail.com>
2026-07-17 20:25:33 +0700

    next

--- app1.py
+++ app1.py
@@ -1,6 +1,7 @@
 import os
 from getpass import getpass
 
+from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 GOOGLE_API_KEY = getpass("Please enter your Google API Key: ")
@@ -10,5 +11,15 @@ os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
 client = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
 
-response = client.invoke("Jelaskan apa itu AI?")
-print(response.text)
+chat_history = [
+    SystemMessage(
+        "You are a helpful chatbot, reply to user chat in short message, max 1 paragraph."
+    )
+]
+
+while True:
+    user_prompt = input("User: ")
+    chat_history.append(HumanMessage(user_prompt))
+    response = client.invoke(chat_history)
+    chat_history.append(response.text)
+    print("AI:", response.text)
