c476f49a48dfff887b66e9f16dc5a31917ae41d0
Mukhlas Adib <adib.rasyidy@gmail.com>
2026-09-04 21:19:02 +0700

    next

--- app1.py
+++ app1.py
@@ -7,14 +7,19 @@ Cara jalanin:
 import os
 from getpass import getpass
 
+from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
 
 GROQ_API_KEY = getpass("Please enter your Groq API Key: ")
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
 print()
-prompt = input("User: ")
-client = ChatGroq(model="openai/gpt-oss-120b")
-response = client.invoke(prompt)
+chat_history = []
 
-print("AI:", response.content)
+while True:
+    prompt = input("User: ")
+    chat_history.append(HumanMessage(prompt))
+    client = ChatGroq(model="openai/gpt-oss-120b")
+    response = client.invoke(prompt)
+    chat_history.append(response)
+    print("AI:", response.content)
